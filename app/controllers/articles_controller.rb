737b2b6da44f12d224d8d3d30130f5e28160c5e2
kamayatsu <50704071+kamayatsu@users.noreply.github.com>
2019-05-23 00:02:58 +0900

    Create new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,23 @@
 class ArticlesController < ApplicationController
   def index
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    Article.create(create_params)
+    redirect_to root_path
+  end
+
+  def show
+  end
+
+  private
+  def create_params
+    params.require(:article).permit(:title, :detail, :image).merge(user_id: current_user.id)
+  end
+
+
 end
