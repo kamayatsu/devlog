d65eaf7e2f167a1c981e1a74fa929f3ba278c347
kamayatsu <50704071+kamayatsu@users.noreply.github.com>
2019-05-25 21:37:08 +0900

    create edit udpate delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,6 +16,22 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def destroy
+    article = Article.find(params[:id])
+    article.destroy if article.user_id == current_user.id
+    redirect_to root_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(create_params) if @article.user_id == current_user.id
+    redirect_to root_path
+  end
+
   private
   def create_params
     params.require(:article).permit(:title, :detail, :image).merge(user_id: current_user.id)
